8021e855ff61ff8fff9a7988331c4f2c2afcaf77
lineDjuissi <linedjuissi@yahoo.com>
2022-10-03 09:01:00 +0200

    spring-security: use users from db instead of memory + bcrypt password management

--- src/main/java/com/notgabs/config/AppConfig.java
+++ src/main/java/com/notgabs/config/AppConfig.java
@@ -1,18 +1,34 @@
 package com.notgabs.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.notgabs")
+@PropertySource("classpath:persistence-mysql.properties")
 public class AppConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private Environment env;
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -28,5 +44,33 @@ public class AppConfig implements WebMvcConfigurer {
         registry
           .addResourceHandler("/resources/**")
           .addResourceLocations("/resources/"); 
+    }
+    
+    @Bean 
+    public DataSource securityDataSource() {
+    	ComboPooledDataSource dataSource = new ComboPooledDataSource();
+    	
+    	try {
+			dataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+    	
+    	dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+    	dataSource.setUser(env.getProperty("jdbc.user"));
+    	dataSource.setPassword(env.getProperty("jdbc.password"));
+    	
+    	dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+    	dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+    	dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+    	dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+    	
+    	return dataSource;
+    }
+    
+    private int getIntProperty(String propertyName) {
+    	String val = env.getProperty(propertyName);
+    	
+    	return Integer.parseInt(val);
     }
 }
--- src/main/java/com/notgabs/config/SecurityConfig.java
+++ src/main/java/com/notgabs/config/SecurityConfig.java
@@ -1,50 +1,49 @@
 package com.notgabs.config;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
-		Collection<UserDetails> users = new ArrayList<>();
-
-		users.add(User.builder().username("john").password("{noop}test").roles("EMPLOYEE").build());
-		users.add(User.builder().username("mary").password("{noop}test").roles("MANAGER").build());
-		users.add(User.builder().username("gabs").password("{noop}test").roles("ADMIN").build());
-
-		return new InMemoryUserDetailsManager(users);
+	@Autowired
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
+
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsManager() {
+//		Collection<UserDetails> users = new ArrayList<>();
+//
+//		users.add(User.builder().username("john").password("{noop}test").roles("EMPLOYEE").build());
+//		users.add(User.builder().username("mary").password("{noop}test").roles("MANAGER").build());
+//		users.add(User.builder().username("gabs").password("{noop}test").roles("ADMIN").build());
+//
+//		return new InMemoryUserDetailsManager(users);
+//	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		// AUTHENTICATION IS REQUIRED FOR ACCESSING THE APP
-		
-		return http.authorizeRequests(configurer -> configurer
-	                .antMatchers("/resources/**").permitAll()
-					.antMatchers("/admin").hasRole("ADMIN")
-					.antMatchers("/manager").hasRole("MANAGER")
-					.anyRequest().authenticated()
-				)
+
+		return http
+				.authorizeRequests(configurer -> configurer.antMatchers("/resources/**").permitAll()
+						.antMatchers("/admin").hasRole("ADMIN").antMatchers("/manager").hasRole("MANAGER").anyRequest()
+						.authenticated())
 				// OVERRIDES THE DEFAULT SPRING LOGIN PAGE
-				.formLogin(configurer -> configurer
-					.loginPage("/login")
-					.loginProcessingUrl("/authenticate").permitAll()
-				)
-				.logout(LogoutConfigurer::permitAll)
-				.exceptionHandling().accessDeniedPage("/error").and()
-				.build();
+				.formLogin(configurer -> configurer.loginPage("/login").loginProcessingUrl("/authenticate").permitAll())
+				.logout(LogoutConfigurer::permitAll).exceptionHandling().accessDeniedPage("/error").and().build();
 	}
 }
